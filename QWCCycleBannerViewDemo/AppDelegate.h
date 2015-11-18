5e8eac66f59c9dd5f1b15a5e94f68c5331fea6ce
tangwilliam <tangwilliam@126.com>
2015-11-18 20:03:24 +0800

    Initial Commit

---
+++ QWCCycleBannerViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QWCCycleBannerViewDemo
+//
+//  Created by 唐秦崴 on 15/11/18.
+//  Copyright © 2015年 tangqinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QWCCycleBannerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QWCCycleBannerViewDemo
+//
+//  Created by 唐秦崴 on 15/11/18.
+//  Copyright © 2015年 tangqinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
